87bb172fb2fe67d63666d1fc73b9cc255b316998
Basmah Salman <basmahsalmann@gmail.com>
2023-09-27 17:46:54 +0300

    help

--- server.go
+++ server.go
@@ -8,11 +8,11 @@ import (
 
 func main() {
 
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.HandleFunc("/form", formHandler)
+	http.Handle("/", fileServer)
+	// http.HandleFunc("/form", formHandler)
 	http.HandleFunc("/ascii-art-web", Handler)
-
-	http.HandleFunc("/ascii-art-web", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hi!")
-	})
 
 	fmt.Printf("Starting the server at port 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
@@ -34,4 +34,17 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 
 	// Bad Request - incorrect request
 	// Internal server error - unhadled errors
+}
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST request successful")
+	name := r.FormValue("name")
+	banner := r.FormValue("banner")
+
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Banner = %s\n", banner)
 }
