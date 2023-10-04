9e27626626167122437332c4f3e7114aa5e94197
fhamza <fatimaaqeel544@gmail.com>
2023-10-04 21:12:54 +0300

    disply at at form.html

--- main/server.go
+++ main/server.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-	//"html/template"
 	"text/template"
+
 	//"html/template"
 	ar "asciiartweb"
 	"log"
@@ -11,44 +11,28 @@ import (
 )
 
 var tmpl *template.Template
+var anss *template.Template
 
 func init() {
 	tmpl = template.Must(template.ParseFiles("../static/index.html"))
+	anss = template.Must(template.ParseFiles("../static/form.html"))
 
 }
 
 type textBanner struct {
-	//converted [][]string
 	converted string
 }
 
 func main() {
 
-	// fileServer := http.FileServer(http.Dir("./static"))
-	//1
 	http.HandleFunc("/", Handler)
-	// http.Handle("/", fileServer)
-	// http.HandleFunc("/form", formHandler)
 	http.HandleFunc("/ascii-art", formHandler)
 
 	fmt.Printf("Starting the server at port 8080\n")
-	//1
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
 }
-
-// func Handler(w http.ResponseWriter, r *http.Request) {
-// if r.URL.Path != "/ascii-art-web" {
-// 	// if template / banner are not found
-// 	http.Error(w, "404 Not Found.", http.StatusNotFound)
-// 	return
-// }
-
-// if r.Method != "GET" {
-// 	http.Error(w, "Method is not supported", http.StatusNotFound)
-// 	return
-// }
 
 // 	// Bad Request 400 - incorrect request
 // 	// blank
@@ -70,12 +54,6 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
-	// if r.URL.Path != "/ascii-art-web" {
-	// 	// if template / banner are not found
-	// 	http.Error(w, "404 Not Found.", http.StatusNotFound)
-	// 	return
-	// }
-
 	// ascii is the converted text we want to print
 	ascii := textBanner{
 		converted: r.FormValue("converted"),
@@ -86,39 +64,49 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	}{true, ascii})
 }
 
-// after this function is written we can run the server
-
-// 2
 // handler for the server
 func formHandler(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
 		fmt.Fprintf(w, "ParseForm() err: %v", err)
 		return
 	}
-	if r.Method != "GET" {
-		http.Error(w, "Method is not supported", http.StatusNotFound)
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method is not supported", http.StatusMethodNotAllowed)
 		return
 	}
 	if r.FormValue("text") == "" {
-		http.Error(w, "400 Bad Request.", http.StatusNotFound)
+		http.Error(w, "400 Bad Request.", http.StatusBadRequest)
 		return
 	}
-	if r.FormValue("banner") != "standard" || r.FormValue("banner") != "shadow" || r.FormValue("banner") != "thinkertoy" {
-		http.Error(w, "500 Internal server error.", http.StatusNotFound)
+	banner := r.Form.Get("banner")
+	// if (banner != "standard") || (banner != "shadow") || (banner != "thinkertoy") {
+	// 	http.Error(w, "500 Internal server error.", http.StatusInternalServerError)
+	// 	return
+	// }
+	switch banner {
+	case "standard":
+	case "shadow":
+	case "thinkertoy":
+		break
+	default:
+		http.Error(w, "500 Internal server error.", http.StatusInternalServerError)
 		return
 	}
 
-	fmt.Fprintf(w, "POST request successful")
+	// fmt.Fprintf(w, "POST request successful")
 
 	text := r.FormValue("text")
-	banner := r.FormValue("banner")
 
-	fmt.Fprintf(w, "Text = %s\n", text)
-	fmt.Fprintf(w, "Banner = %s\n", banner)
+	// fmt.Fprintf(w, "Text = %s\n", text)
+	// fmt.Fprintf(w, "Banner = %s\n", banner)
 
 	// Call Validating Inputs
 	ar.ValidatingInput(text)
 
 	// Calling Storing Function
-	ar.PrintArray(text, banner)
+	ans := ar.PrintArray(text, banner)
+	// fmt.Print(ans)
+	//fmt.Fprint(w, ans)
+
+	anss.Execute(w, ans)
 }
